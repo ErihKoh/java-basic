2b6536e15850cfde4d9dd8a681b85f704d0fc4df
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-08-30 19:03:56 +0300

    created project carddeck

---
+++ cardDeck/src/main/java/learning/carddeck/CardDeck.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package learning.carddeck;
+
+/**
+ *
+ * @author Admin
+ */
+public class CardDeck {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
