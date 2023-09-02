6a0ebc6d94e6727966b2838a13ff65e11a1cee31
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-09-02 18:57:05 +0300

    completed

--- cardDeck/src/main/java/learning/carddeck/Deck.java
+++ cardDeck/src/main/java/learning/carddeck/Deck.java
@@ -9,9 +9,9 @@ public class Deck {
     public Card[] cards_deck = new Card[CARDS_COUNT];
 
     public Deck() {
-        for (int i = 0; i < Rank.values.length; i += 1) {
-            for (int j = 0; j < Suit.values.length; j += 1) {
-                cards_deck[cardNumber] = new Card(Rank.values[i], Suit.values[j]);
+        for (int i = 0; i < Suit.values.length; i += 1) {
+            for (int j = 0; j < Rank.values.length; j += 1) {
+                cards_deck[cardNumber] = new Card(Rank.values[j], Suit.values[i]);
                 cardNumber+=1;
             }
         }
@@ -20,9 +20,9 @@ public class Deck {
     
     //Перемішує колоду у випадковому порядку 
     public void shuffle() {
-        for (int i = cards_deck.length - 1; i > 0; i--) {
-            int j = (int) Math.floor(Math.random() * (i + 1));
-            
+        for (int i = cards_deck.length - 1; i > 0; i-=1) {
+            int j = (int) Math.floor(Math.random() * (i + 1)); // Тасование 
+                                                              //Фишера — Йетса.
             Card temp = cards_deck[i];
             cards_deck[i] = cards_deck[j];
             cards_deck[j] = temp;
@@ -45,17 +45,34 @@ public class Deck {
     * HEARTS 6 
     * І так далі для DIAMONDS, CLUBS, SPADES */
     public void order() {
+        for (int i = 0; i < Suit.values.length; i += 1) {
+            for (int j = 0; j < Rank.values.length; j += 1) {
+                cards_deck[cardNumber] = new Card(Rank.values[j], Suit.values[i]);
+                cardNumber+=1;
+            }
+        }
+    
     }
     
     //Повертає true у випадку коли в колоді ще доступні карти
     public boolean hasNext() {
-        return false;
+        if (cardNumber <= 0) return false;
+       
+        return true;
     }
     
     //"Виймає" одну карту з колоди, коли буде видано всі 36 карт повертає null
     //Карти виймаються з "вершини" колоди. Наприклад перший виклик видасть SPADES 6 потім
     //SPADES 7, ..., CLUBS 6, ..., CLUBS Ace і так далі до HEARTS Ace
     public Card drawOne() {
-        return null;
-    } 
+        if (!hasNext()) {
+            return null;
+        } else {
+            Card card = cards_deck[cardNumber - 1];
+            cardNumber-=1;
+            return card;
+        }
+
+    }
+    
 }
