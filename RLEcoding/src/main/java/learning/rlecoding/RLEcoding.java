df529c25c224eed8b9270daf7028ca0c7c36cd77
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-09-29 14:29:51 +0300

    rlecoding

--- RLEcoding/src/main/java/learning/rlecoding/RLEcoding.java
+++ RLEcoding/src/main/java/learning/rlecoding/RLEcoding.java
@@ -4,30 +4,30 @@ import java.util.Arrays;
 
 
 public class RLEcoding {
-
-    public static String encoding(String str) {
-        String[] arr;
-        int counter = 0;
-        String temp;
-        if (str == null && "".equals(str)) return "1";
-        else arr = str.split("");
-        
-        for (String item : arr) {
-            temp = item;
-            if (temp.equals(item)) {
-                counter++;
-            } else {
-                String newString = temp + counter;
-                counter = 0;
-                System.out.println(newString);
-            }
-        }
-        
-        
-        return "";
-    }
     
     public static void main(String[] args) {
-        encoding("");
-    }
+        
+        if (args != null && args.length > 0) {
+            
+            String temp;
+            
+            StringBuilder strBuild = new
+            
+            int counter = 0;
+            
+            String[] strArr = args[0].split("");
+            temp = strArr[0];
+            for (String item: strArr) {
+                if (temp == item) {
+                    counter++;
+                    if (counter == 9) {
+                        
+                    }
+                }
+                
+            }
+            
+            
+        } else System.out.println("");
+    }    
 }
