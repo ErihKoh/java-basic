713e50301b06399e122e7a849e9beee235e1efd7
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-10-06 19:06:40 +0300

    create insertionSort

---
+++ insertionSort/src/main/java/learning/insertionsort/InsertionSort.java
@@ -0,0 +1,29 @@
+package learning.insertionsort;
+
+import java.util.Arrays;
+
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        System.out.println(insertion_sort(arr));
+    }
+    
+    public static String insertion_sort(int[] array){
+        int counter = 0;
+        for ( int i = 1; i < array.length; i++ ){
+            int key = array[i];
+            int idx = i - 1;
+
+            while(idx >= 0 && array[idx] > key){
+                array[idx + 1] = array[idx];
+                idx = idx - 1;
+                counter ++;
+            }
+            array[idx + 1] = key;
+        }
+        System.out.println(counter);
+        return Arrays.toString(array);
+    }
+}
