1eb5c72f35b383a262d739143a88cae11639b91f
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-10-06 20:03:02 +0300

    fix sort

--- bubleSort/src/main/java/learning/bublesort/BubleSort.java
+++ bubleSort/src/main/java/learning/bublesort/BubleSort.java
@@ -4,22 +4,26 @@ package learning.bublesort;
 public class BubleSort {
 
     public static void main(String[] args) {
-        int[] array = {30, 2, 10, 4, 6};
+        int counter = 0;
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         int length = array.length;
         int temp;
-        for (int i = 0; i < length - 1; i+=1) {
-            for (int j = length - 1; j > i; j--) {
+        for (int i = 0; i < length; i+=1) {
+            for (int j = 1; j < length - i; j++) {
                 if(array[j - 1] > array[j]) {
                     temp = array[j - 1];
                     array[j - 1] = array[j];
                     array[j] = temp;
                 }
+                counter ++;
             }
         }
 
         for (int i = 0; i < length; i++) {
             System.out.print(array[i] + " ");
         }
+        
+        System.out.println(counter);
                 
                 
     }
